1ae5f7d96ca067511d2fe094620406d9a8d78896
MFW-1 <am64fw5@gmail.com>
2026-05-20 10:59:22 +0500

    2

---
+++ HMSD/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using HMSD.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AdminController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    // PROFILE PAGE
+    public async Task<IActionResult> Profile()
+    {
+        var user = await _userManager.GetUserAsync(User);
+
+        return View(user);
+    }
+
+    // UPDATE PROFILE
+    [HttpPost]
+    public async Task<IActionResult> Profile(ApplicationUser model)
+    {
+        var user = await _userManager.GetUserAsync(User);
+
+        if (user == null)
+            return NotFound();
+
+        user.FullName = model.FullName;
+        user.Email = model.Email;
+        user.UserName = model.Email;
+        user.PhoneNumber = model.PhoneNumber;
+
+        await _userManager.UpdateAsync(user);
+
+        ViewBag.Success = "Profile updated successfully";
+
+        return View(user);
+    }
+}
